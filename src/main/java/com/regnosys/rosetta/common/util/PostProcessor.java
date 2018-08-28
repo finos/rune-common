f9a17b60fbc6625ab6444a666898e558b50a104c
Minesh Patel <minesh.patel@regnosys.com>
2018-08-28 17:50:04 +0100

    Removed static constants and replaced bt generic method

--- src/main/java/com/regnosys/rosetta/common/util/PostProcessor.java
+++ src/main/java/com/regnosys/rosetta/common/util/PostProcessor.java
@@ -1,13 +1,13 @@
 package com.regnosys.rosetta.common.util;
 
-import com.rosetta.model.lib.RosettaModelObject;
-
 import java.util.Collections;
 import java.util.List;
 
 public interface PostProcessor<T> {
 
-    PostProcessor<RosettaModelObject> NO_OP_POST_PROCESSOR = (modelObject) -> Collections.emptyList();
-
     List<PathValue> process(T t);
+
+    static <T> PostProcessor<T> empty() {
+        return x -> Collections.emptyList();
+    }
 }

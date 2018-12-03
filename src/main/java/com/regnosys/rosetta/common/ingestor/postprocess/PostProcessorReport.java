426a831eddd6a20ae45e0045629e3ecd0d07034e
Jim Wang <jim.wang@regnosys.com>
2018-12-03 11:38:39 +0000

    Move Post Processor from common into lib

--- src/main/java/com/regnosys/rosetta/common/ingestor/postprocess/PostProcessorReport.java
+++
@@ -1,8 +0,0 @@
-package com.regnosys.rosetta.common.ingestor.postprocess;
-
-/**
- * A marker interface to identify output of {@link PostProcessor}s. {@link PostProcessorReport}s can contain
- * arbitrary data.
- */
-public interface PostProcessorReport {
-}

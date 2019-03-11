91ff5e14d0da495a5a184810ef9ba372a84872b5
Nikos Sotiropoulos <nikos.sotiropoulos@regnosys.com>
2019-03-11 15:46:00 +0000

    Added a more general version of objectMapper; append our introspector by module registering

--- src/main/java/com/regnosys/rosetta/common/serialisation/RosettaObjectMapper.java
+++ src/main/java/com/regnosys/rosetta/common/serialisation/RosettaObjectMapper.java
@@ -4,13 +4,20 @@ import java.lang.reflect.Modifier;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.json.PackageVersion;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.rosetta.model.lib.RosettaModelObject;
 
 /**
@@ -22,13 +29,15 @@ public class RosettaObjectMapper {
 	private RosettaObjectMapper() {
 	}
 
-	// until we find how to properly configure a common one with all the introspector logic we need
-	// we will use a double
-	private static class LazyHolderForDropWizard {
-		static final ObjectMapper INSTANCE = new ObjectMapper()
-																.findAndRegisterModules()
-																.registerModule(new JavaTimeModule())
+	private static class LazyHolder {
+		static final ObjectMapper INSTANCE = new ObjectMapper()	.findAndRegisterModules()
+																.registerModule(new GuavaModule())
+																.registerModule(new JodaModule())
+																.registerModule(new AfterburnerModule())
+																.registerModule(new ParameterNamesModule())
 																.registerModule(new Jdk8Module())
+																.registerModule(new JavaTimeModule())
+																.registerModule(new RosettaModule())
 																.setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
 																.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 																.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
@@ -37,26 +46,37 @@ public class RosettaObjectMapper {
 																.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
 	}
 
-	private static class LazyHolder {
-		static final ObjectMapper INSTANCE = new ObjectMapper()
-																.findAndRegisterModules()
-																.registerModule(new JavaTimeModule())
-																.registerModule(new Jdk8Module())
-																.setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
-																.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-																.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
-																.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true)
-																.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-																.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
-																.setAnnotationIntrospector(new RosettaBuilderIntrospector());
-	}
-
 	public static ObjectMapper getDefaultRosettaObjectMapper() {
 		return LazyHolder.INSTANCE;
 	}
 
-	public static ObjectMapper forDropWizard() {
-		return LazyHolderForDropWizard.INSTANCE;
+	/**
+	 * Using a module class to append our annotation introspector with a minimal fuss 
+	 * 
+	 */
+	private static class RosettaModule extends SimpleModule {
+		
+		private static final long serialVersionUID = 1L;
+
+		public RosettaModule() {
+			super(PackageVersion.VERSION);
+		}
+
+		@Override
+		public void setupModule(SetupContext context) {
+			super.setupModule(context);
+			context.insertAnnotationIntrospector(new RosettaBuilderIntrospector());
+		}
+
+		@Override
+		public int hashCode() {
+			return getClass().hashCode();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			return this == o;
+		}
 	}
 
 	private static class RosettaBuilderIntrospector extends JacksonAnnotationIntrospector {
@@ -68,25 +88,27 @@ public class RosettaObjectMapper {
 
 		@Override
 		public Class<?> findPOJOBuilder(AnnotatedClass ac) {
-			Class<?> rawClass = ac	.getType()
-									.getRawClass();
+			JavaType type = ac.getType();
+			// [Ljava.lang.String  type is null!
+			if (null != type) {
+				Class<?> rawClass = type.getRawClass();
 
-			if (!Modifier.isAbstract(rawClass.getModifiers()) && RosettaModelObject.class.isAssignableFrom(rawClass)) {
-				try {
-					return Class.forName(rawClass.getTypeName() + "$" + rawClass.getSimpleName() + "Builder",
-							true, rawClass.getClassLoader());
+				if (!Modifier.isAbstract(rawClass.getModifiers()) && RosettaModelObject.class.isAssignableFrom(rawClass)) {
+					try {
+						return Class.forName(rawClass.getTypeName() + "$" + rawClass.getSimpleName() + "Builder",
+								true, rawClass.getClassLoader());
 
-				} catch (ClassNotFoundException e) {
-					throw new RosettaSerialiserException("Could not find the builder class for " + rawClass, e);
+					} catch (ClassNotFoundException e) {
+						throw new RosettaSerialiserException("Could not find the builder class for " + rawClass, e);
+					}
 				}
 			}
-
 			return super.findPOJOBuilder(ac);
 		}
 
 		@Override
 		public Version version() {
-			return null;
+			return Version.unknownVersion();
 		}
 	}
 

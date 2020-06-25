a0b3c0a87e5b848d388e31304cee1fc9f721eee2
Hugo Hills <hugo.hills@regnosys.com>
2020-06-25 16:21:53 +0100

    Separate mapper and builderProcessor to help with testing

---
+++ src/main/java/com/regnosys/rosetta/common/translation/MappingDelegate.java
@@ -0,0 +1,22 @@
+package com.regnosys.rosetta.common.translation;
+
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.path.RosettaPath;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MappingDelegate {
+
+	void map(Path synonymPath, RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent);
+
+	void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent);
+
+	<T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent);
+
+	<T> void mapBasic(Path synonymPath, List<T> instance, RosettaModelObjectBuilder parent);
+
+	RosettaPath getModelPath();
+
+	List<Path> getSynonymPaths();
+}

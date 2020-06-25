7c5b74e0e9a987ba77ac31f41888d30949bc1391
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2020-06-25 17:56:24 +0100

    Separate mapper and builderProcessor to help with testing (#94)
    * Fix mapping processor path comparison
    
    * Separate mapper and builderProcessor to help with testing
    
    * Make delegate interface consistent
    
    * Fix method typo

---
+++ src/main/java/com/regnosys/rosetta/common/translation/MappingBuilderProcessor.java
@@ -0,0 +1,79 @@
+package com.regnosys.rosetta.common.translation;
+
+import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.meta.ReferenceWithMeta;
+import com.rosetta.model.lib.path.RosettaPath;
+import com.rosetta.model.lib.process.AttributeMeta;
+import com.rosetta.model.lib.process.BuilderProcessor;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implements BuilderProcessor and delegates to the given MappingProcessor when the path matches.
+ */
+@SuppressWarnings("unused") // Used in xtend
+public class MappingBuilderProcessor implements BuilderProcessor {
+
+	private final MappingDelegate delegate;
+	private final RosettaPath modelPath;
+	private final List<Path> synonymPaths;
+
+	public MappingBuilderProcessor(MappingDelegate delegate) {
+		this.delegate = delegate;
+		this.modelPath = delegate.getModelPath();
+		this.synonymPaths = delegate.getSynonymPaths();
+	}
+
+	@Override
+	public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath,
+			Class<? extends R> rosettaType,
+			RosettaModelObjectBuilder builder,
+			RosettaModelObjectBuilder parent,
+			AttributeMeta... meta) {
+		if (currentPath.equals(modelPath)) {
+			synonymPaths.forEach(p -> delegate.map(p, Optional.ofNullable(builder), parent));
+		}
+		return true;
+	}
+
+	@Override
+	public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath,
+			Class<? extends R> rosettaType,
+			List<? extends RosettaModelObjectBuilder> builder,
+			RosettaModelObjectBuilder parent,
+			AttributeMeta... meta) {
+		if (matchesProcessorPathForMultipleCardinality(currentPath, rosettaType)) {
+			synonymPaths.forEach(p -> delegate.map(p, Optional.ofNullable(builder).orElse(Collections.emptyList()), parent));
+		}
+		return true;
+	}
+
+	@Override
+	public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, T instance, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
+		if (currentPath.equals(modelPath)) {
+			synonymPaths.forEach(p -> delegate.mapBasic(p, Optional.ofNullable(instance), parent));
+		}
+	}
+
+	@Override
+	public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, List<T> instance, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
+		if (currentPath.equals(modelPath)) {
+			synonymPaths.forEach(p -> delegate.mapBasic(p, Optional.ofNullable(instance).orElse(Collections.emptyList()), parent));
+		}
+	}
+
+	@Override
+	public BuilderProcessor.Report report() {
+		return null;
+	}
+
+	private boolean matchesProcessorPathForMultipleCardinality(RosettaPath currentPath, Class<?> rosettaType) {
+		return ReferenceWithMeta.class.isAssignableFrom(rosettaType) ?
+				// so the parse handlers match on the list rather than each list item
+				currentPath.equals(modelPath.getParent()) :
+				currentPath.equals(modelPath);
+	}
+}
--- src/main/java/com/regnosys/rosetta/common/translation/MappingProcessor.java
+++ src/main/java/com/regnosys/rosetta/common/translation/MappingProcessor.java
@@ -1,18 +1,17 @@
 package com.regnosys.rosetta.common.translation;
 
-import com.rosetta.model.lib.RosettaModelObject;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
-import com.rosetta.model.lib.meta.ReferenceWithMeta;
 import com.rosetta.model.lib.path.RosettaPath;
-import com.rosetta.model.lib.process.AttributeMeta;
-import com.rosetta.model.lib.process.BuilderProcessor;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-public abstract class MappingProcessor implements BuilderProcessor {
+/**
+ * Base implementation does not do any mapping, but each mapping method can be overridden.
+ */
+public abstract class MappingProcessor implements MappingDelegate {
 
 	private final RosettaPath modelPath;
 	private final List<Path> synonymPaths;
@@ -27,85 +26,40 @@ public abstract class MappingProcessor implements BuilderProcessor {
 	}
 
 	@Override
-	public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath,
-			Class<? extends R> rosettaType,
-			RosettaModelObjectBuilder builder,
-			RosettaModelObjectBuilder parent,
-			AttributeMeta... meta) {
-		if (builder != null && currentPath.matchesIgnoringIndex(modelPath)) {
-			synonymPaths.forEach(p -> map(p, builder, parent));
-		}
-		return true;
+	public void map(Path synonymPath, Optional<RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
+		builder.ifPresent(b -> map(synonymPath, b, parent));
 	}
 
-	@Override
-	public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath,
-			Class<? extends R> rosettaType,
-			List<? extends RosettaModelObjectBuilder> builder,
-			RosettaModelObjectBuilder parent,
-			AttributeMeta... meta) {
-		if (builder != null && matchesProcessorPathForMultipleCardinality(currentPath, rosettaType)) {
-			synonymPaths.forEach(p -> map(p, builder, parent));
-		}
-		return true;
-	}
-
-	@Override
-	public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, T instance, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
-		if (currentPath.matchesIgnoringIndex(modelPath)) {
-			synonymPaths.forEach(p -> mapBasic(p, Optional.ofNullable(instance), parent));
-		}
-	}
-
-	@Override
-	public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, List<T> instance, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
-		if (instance != null && currentPath.matchesIgnoringIndex(modelPath)) {
-			synonymPaths.forEach(p -> mapBasic(p, instance, parent));
-		}
-	}
-
-	@Override
-	public Report report() {
-		return null;
-	}
-
-	/**
-	 * Perform custom mapping logic and updates resultant mapped value on builder object.
-	 */
-	protected void map(Path synonymPath, RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
+	public void map(Path synonymPath, RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
 		// Default behaviour - do nothing
 	}
 
-	/**
-	 * Perform custom mapping logic and updates resultant mapped value on builder object.
-	 */
-	protected void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
+	@Override
+	public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
 		// Default behaviour - do nothing
 	}
 
-	/**
-	 * Perform custom mapping logic and updates resultant mapped value on builder object.
-	 */
-	protected <T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
-		instance.ifPresent(inst -> mapBasic(synonymPath, inst, parent));
+	@Override
+	public <T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
+		instance.ifPresent(i -> mapBasic(synonymPath, i, parent));
 	}
 
-	protected <T> void mapBasic(Path synonymPath, T instance, RosettaModelObjectBuilder parent) {
+	public <T> void mapBasic(Path synonymPath, T instance, RosettaModelObjectBuilder parent) {
 		// Default behaviour - do nothing
 	}
 
-	/**
-	 * Perform custom mapping logic and updates resultant mapped value on builder object.
-	 */
-	protected <T> void mapBasic(Path synonymPath, List<T> instance, RosettaModelObjectBuilder parent) {
+	@Override
+	public <T> void mapBasic(Path synonymPath, List<T> instance, RosettaModelObjectBuilder parent) {
 		// Default behaviour - do nothing
 	}
 
-	protected RosettaPath getModelPath() {
+	@Override
+	public RosettaPath getModelPath() {
 		return modelPath;
 	}
 
-	protected List<Path> getSynonymPaths() {
+	@Override
+	public List<Path> getSynonymPaths() {
 		return synonymPaths;
 	}
 
@@ -123,12 +77,5 @@ public abstract class MappingProcessor implements BuilderProcessor {
 
 	protected void setValueAndUpdateMappings(Path synonymPath, Consumer<String> setter) {
 		MappingProcessorUtils.setValueAndUpdateMappings(synonymPath, setter, mappings, modelPath);
-	}
-
-	private boolean matchesProcessorPathForMultipleCardinality(RosettaPath currentPath, Class<?> rosettaType) {
-		return ReferenceWithMeta.class.isAssignableFrom(rosettaType) ?
-				// so the parse handlers match on the list rather than each list item
-				currentPath.matchesIgnoringIndex(modelPath.getParent()) :
-				currentPath.matchesIgnoringIndex(modelPath);
 	}
 }

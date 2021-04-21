7ea55c924f44eaea16b7ed6af125b8b8dc2e6004
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2021-04-21 10:35:52 +0100

    Add flat file mapping utils (#153)

---
+++ src/main/java/com/regnosys/rosetta/common/translation/flat/FlatFileMappingProcessor.java
@@ -0,0 +1,186 @@
+package com.regnosys.rosetta.common.translation.flat;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.regnosys.rosetta.common.translation.Mapping;
+import com.regnosys.rosetta.common.translation.MappingContext;
+import com.regnosys.rosetta.common.translation.MappingProcessor;
+import com.regnosys.rosetta.common.translation.Path;
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.path.RosettaPath;
+import com.rosetta.model.lib.records.Date;
+import com.rosetta.model.lib.records.DateImpl;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+
+/**
+ * Mapping processor base type specialised for flat, or shallow, xml files where all fields are translated
+ * by a single mapping processor rather than synonyms.
+ */
+public abstract class FlatFileMappingProcessor<TYPE extends RosettaModelObjectBuilder> extends MappingProcessor {
+
+	protected static final Path BASE_PATH = Path.parse("WorkflowStep");
+
+	protected final DateTimeFormatter dateParser = DateTimeFormatter.BASIC_ISO_DATE;
+	protected final DateTimeFormatter localTimeParser = DateTimeFormatter.ISO_LOCAL_TIME;
+	protected final DateTimeFormatter localDateParser = DateTimeFormatter.ISO_LOCAL_DATE;
+
+	@FunctionalInterface
+	protected interface MappingConsumer<T> {
+		List<PathValue<?>> accept(Map<String, Integer> indexes, String xmlValue, PathValue<T> pathValue);
+	}
+
+	protected static class PathValue<T> {
+		private final Path modelPath;
+		private final T value;
+		private final boolean conditional;
+
+		public PathValue(Path modelPath, T value) {
+			this(modelPath, value, false);
+		}
+
+		public PathValue(Path modelPath, T value, boolean conditional) {
+			this.modelPath = modelPath;
+			this.value = value;
+			this.conditional = conditional;
+		}
+
+		public Path getModelPath() {
+			return modelPath;
+		}
+
+		public T getValue() {
+			return value;
+		}
+	}
+
+	private Multimap<IndexCapturePath, IndexCapturePath> pathLookup = ArrayListMultimap.create();
+	private Multimap<IndexCapturePath, MappingConsumer<TYPE>> mappings = HashMultimap.create();
+	private Multimap<String, Capture> captures = HashMultimap.create();
+	private Collection<BiConsumer<Multimap<String, Capture>, TYPE>> postCaptureProcessors = new ArrayList<>();
+
+	public FlatFileMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
+		super(modelPath, synonymPaths, context);
+	}
+
+	protected BigDecimal parseDecimal(String value) {
+		return new BigDecimal(value);
+	}
+
+	protected Date parseISODate(String value) {
+		return new DateImpl(LocalDate.parse(value, dateParser));
+	}
+	protected Date parseLocalDate(String value) {
+		return new DateImpl(LocalDate.parse(value, localDateParser));
+	}
+
+	@Override
+	public void map(Path synonymPath, Optional<RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
+		@SuppressWarnings("unchecked")
+		TYPE type = (TYPE) parent;
+		Set<Mapping> inputs = new HashSet(this.getContext().getMappings());
+		doHardCodings(type);
+
+		List<Mapping> allMappings = new ArrayList<>();
+		for (Mapping m : inputs) {
+			String xmlPath = m.getXmlPath().toString();
+			IndexCapturePath xmlCapturing = IndexCapturePath.parse(xmlPath);
+			Collection<IndexCapturePath> capturers = pathLookup.get(xmlCapturing.toUnindexed());
+			boolean mapped = false;
+			for (IndexCapturePath capturer : capturers) {
+				if (xmlCapturing.matches(capturer)) {
+					Map<String, Integer> captureIndexes = capturer.captureIndexes(xmlCapturing);
+					//The way we build paths for Json files is broken so it just shoves the indexes at the end of the path - so I am going to capture that index as "other"
+					xmlCapturing.getLastIndex().ifPresent(i -> captureIndexes.put("other", i));
+					Collection<MappingConsumer<TYPE>> mappingConsumers = mappings.get(capturer);
+					String xmlValue = m.getXmlValue() == null ? null : m.getXmlValue().toString();
+					for (MappingConsumer<TYPE> mc : mappingConsumers) {
+						List<PathValue<?>> results = mc.accept(captureIndexes, xmlValue, new PathValue<>(BASE_PATH, type));
+						for (PathValue<?> r : results) {
+							if (xmlValue != null) {
+								allMappings.add(new Mapping(m.getXmlPath(), xmlValue, r.modelPath, r.value,null,true, r.conditional));
+							}
+						}
+					}
+					mapped = true;
+				}
+			}
+			if (!mapped) {
+				allMappings.add(m);
+			}
+		}
+		doConditionalStuff(type);
+		updateMappings(allMappings);
+	}
+
+	private void updateMappings(List<Mapping> allMappings) {
+		this.getContext().getMappings().clear();
+		this.getContext().getMappings().addAll(allMappings);
+	}
+
+	@Override
+	public <T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
+		super.map(synonymPath, Optional.empty(), parent);
+	}
+
+	@Override
+	public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder,
+			RosettaModelObjectBuilder parent) {
+		super.map(synonymPath, Optional.empty(), parent);
+	}
+
+	protected abstract void doHardCodings(TYPE object);
+
+	protected MappingConsumer<TYPE> nonNullConsumer(MappingConsumer<TYPE> consumer) {
+		return (i, v, r) -> v != null ? consumer.accept(i, v, r) : List.of();
+	}
+
+	private void doConditionalStuff(TYPE workflow) {
+		for (BiConsumer<Multimap<String, Capture>, TYPE> processor:postCaptureProcessors) {
+			processor.accept(captures, workflow);
+		}
+	}
+
+	protected <T> MappingConsumer<T> capture(String name) {
+		return (indexes, value, workflow) -> {
+			captures.put(name, new Capture(indexes, value));
+			return List.of(new PathValue<>(workflow.getModelPath(), value, true));
+		};
+	}
+
+	protected void addMapping(IndexCapturePath path, MappingConsumer<TYPE> consumer) {
+		pathLookup.put(path.toUnindexed(), path);
+		mappings.put(path, consumer);
+	}
+
+	protected void addPostCaptureProcessors(BiConsumer<Multimap<String, Capture>, TYPE> postCaptureProcessor) {
+		postCaptureProcessors.add(postCaptureProcessor);
+	}
+	
+	protected <A> Optional<A> any(Collection<A> collection) {
+		return collection.stream().findAny();
+	}
+	
+	protected Optional<Capture> matchingIndex(Capture toMatch, Collection<Capture> lookIn, String... matchOn) {
+		Map<String, Integer> matchValues = Arrays.stream(matchOn).collect(Collectors.toMap(k->k, k->toMatch.getIndexes().get(k)));
+		return lookIn.stream().filter(c->matches(c,matchValues)).findFirst();
+	}
+
+	private boolean matches(Capture c, Map<String, Integer> matchValues) {
+		Map<String, Integer> indexes = c.getIndexes();
+		for (Entry<String, Integer> matchValue : matchValues.entrySet()) {
+			Integer val = indexes.get(matchValue.getKey());
+			if (!val.equals(matchValue.getValue())) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/main/java/com/regnosys/rosetta/common/translation/flat/IndexCapturePath.java
@@ -0,0 +1,208 @@
+package com.regnosys.rosetta.common.translation.flat;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class IndexCapturePath implements Comparable<IndexCapturePath> {
+
+	private final IndexCapturePathElement[] elements; 
+	
+	public static IndexCapturePath parse(String s) {
+		String[] splits = s.split("\\.");
+		IndexCapturePathElement[] elements = Arrays.stream(splits).map(e->IndexCapturePathElement.parse(e)).toArray(i->new IndexCapturePathElement[i]);
+		return new IndexCapturePath(elements);
+	}
+
+	private IndexCapturePath(IndexCapturePathElement[] elements) {
+		this.elements = elements;
+	}
+	
+	public IndexCapturePath toUnindexed() {
+		return new IndexCapturePath(Arrays.stream(elements).map(e->e.toUnindexed()).toArray(i->new IndexCapturePathElement[i]));
+	}
+	
+	public Map<String, Integer> captureIndexes(IndexCapturePath indexed) {
+		if (!this.matches(indexed)) throw new RuntimeException("Paths must match");
+		Map<String, Integer> result = new HashMap<>();
+		for (int i=0;i<elements.length;i++) {
+			elements[i].captureIdex(result, indexed.elements[i]);
+		}
+		return result;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(elements);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IndexCapturePath other = (IndexCapturePath) obj;
+		if (!Arrays.equals(elements, other.elements))
+			return false;
+		return true;
+	}
+	
+	public boolean matches(IndexCapturePath other) {
+		if (this == other)
+			return true;
+		if (other == null)
+			return false;
+		if (elements.length!=other.elements.length)
+			return false;
+		for (int i=0;i<elements.length;i++) {
+			if (!elements[i].matches(other.elements[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int compareTo(IndexCapturePath o) {
+		for (int i=0;i<Math.min(this.elements.length, o.elements.length);i++) {
+			int elCompare = elements[i].compareTo(o.elements[i]);
+			if (elCompare!=0) return elCompare;
+		}
+		int lengthDif = elements.length - o.elements.length;
+		return lengthDif;
+	}
+
+	@Override
+	public String toString() {
+		return "IndexCapturePath [" + Arrays.stream(elements).map(e->e.toString()).collect(Collectors.joining(".")) + "]";
+	}
+
+	public static class IndexCapturePathElement implements Comparable<IndexCapturePathElement>{
+		private final String elementName;
+		private final Optional<String> indexCaptureName;
+		private final Optional<Integer> capturedIndex;
+		
+		//this pattern matches an element name (containing any characters except [ and (
+		//followed by an optional index term consisting of a bracket ( or [
+		//then either an index label (java indentifier rules) or an index number
+		//e.g activity or activity[1] or activity[activityNum] or activity(1) or activity(activityNum) 
+		private static final Pattern elementPattern = Pattern.compile("(?<name>[^\\[(]*)((\\(|\\[)((?<capture>[A-Za-z_]+[A-Za-z0-9_]*)|(?<index>[0-9]+))(\\)|\\]))?");
+		
+		public static IndexCapturePathElement parse(String s) {
+			Matcher m= elementPattern.matcher(s);
+			m.matches();
+			String name = m.group("name");
+			String capture = m.group("capture");
+
+			if ("".equals(capture)) capture=null;
+			String index = m.group("index");
+			if ("".equals(index)) index=null;
+			Optional<Integer> indexOp = Optional.ofNullable(index).map(Integer::parseInt);
+			return new IndexCapturePathElement(name, Optional.ofNullable(capture), indexOp);
+		}
+		
+		public void captureIdex(Map<String, Integer> result, IndexCapturePathElement other) {
+			if (indexCaptureName.isPresent() && other.capturedIndex.isPresent()) {
+				result.put(indexCaptureName.get(), other.capturedIndex.get());
+			}
+		}
+		
+		public IndexCapturePathElement toUnindexed() {
+			return new IndexCapturePathElement(elementName, Optional.empty(), Optional.empty());
+		}
+
+		private IndexCapturePathElement(String elementName, Optional<String> indexCaptureName, Optional<Integer> capturedIndex) {
+			this.elementName = elementName;
+			this.indexCaptureName = indexCaptureName;
+			this.capturedIndex = capturedIndex;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((capturedIndex == null) ? 0 : capturedIndex.hashCode());
+			result = prime * result + ((elementName == null) ? 0 : elementName.hashCode());
+			result = prime * result + ((indexCaptureName == null) ? 0 : indexCaptureName.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			IndexCapturePathElement other = (IndexCapturePathElement) obj;
+			if (capturedIndex == null) {
+				if (other.capturedIndex != null)
+					return false;
+			} else if (!capturedIndex.equals(other.capturedIndex))
+				return false;
+			if (elementName == null) {
+				if (other.elementName != null)
+					return false;
+			} else if (!elementName.equals(other.elementName))
+				return false;
+			if (indexCaptureName == null) {
+				if (other.indexCaptureName != null)
+					return false;
+			} else if (!indexCaptureName.equals(other.indexCaptureName))
+				return false;
+			return true;
+		}
+
+		public boolean matches(IndexCapturePathElement other) {
+			if (this == other)
+				return true;
+			if (other == null)
+				return false;
+			if (elementName == null) {
+				if (other.elementName != null)
+					return false;
+			} else if (!elementName.equals(other.elementName))
+				return false;
+			if (capturedIndex.isPresent()) {
+				if (other.indexCaptureName.isPresent()) {//a number index always matches with an index label
+					return true;
+				}
+				else if (other.capturedIndex.isPresent()) {//if the index number is specified on both then they have to match
+					return this.capturedIndex.equals(other.capturedIndex);
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public int compareTo(IndexCapturePathElement o) {
+			return elementName.compareTo(o.elementName);
+		}
+
+		@Override
+		public String toString() {
+			return elementName + indexCaptureName.map(c->"["+c+"]").orElse("")
+					+ capturedIndex.map(c->"["+c+"]").orElse("");
+		}
+	}
+
+	public Optional<Integer> getLastIndex() {
+		for (int i=elements.length-1;i>=0;i--) {
+			if (elements[i].capturedIndex.isPresent()) {
+				return elements[i].capturedIndex;
+			}
+		}
+		return Optional.empty();
+	}
+}

3056de4fdd2032220b88773d9294500a7888fa18
David Al-Kanani <david.al-kanani@regnosys.com>
2022-05-09 09:23:03 +0100

    Issue 201 - create utility to parse timestamps for ui (#224)
    * create formatter utility for toString data value objects
    
    * use utility to format date values for reporting

---
+++ src/main/java/com/regnosys/rosetta/common/serialisation/RosettaDataValueObjectToString.java
@@ -0,0 +1,17 @@
+package com.regnosys.rosetta.common.serialisation;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class RosettaDataValueObjectToString {
+
+    public static String toValueString(Object object) {
+        if (object instanceof ZonedDateTime) {
+            ZonedDateTime zonedDateTime = (ZonedDateTime) object;
+            return zonedDateTime.truncatedTo(ChronoUnit.SECONDS)
+                    .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+        return object.toString();
+    }
+}

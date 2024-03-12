ffeb45bf1ef722540d56dd299ff07288a3f7956a
David Al-Kanani <david.al-kanani@regnosys.com>
2024-03-12 15:18:46 +0000

    add applicable reports to ProjectionDataSet

--- src/main/java/com/regnosys/rosetta/common/serialisation/projectiondata/ProjectionDataSet.java
+++ src/main/java/com/regnosys/rosetta/common/serialisation/projectiondata/ProjectionDataSet.java
@@ -2,63 +2,53 @@ package com.regnosys.rosetta.common.serialisation.projectiondata;
 
 import com.regnosys.rosetta.common.serialisation.DataSet;
 import com.regnosys.rosetta.common.serialisation.reportdata.ReportDataItem;
+import com.rosetta.model.lib.ModelReportId;
 
 import java.util.*;
 
 public class ProjectionDataSet extends DataSet {
 
-    private String applicableProjection;
+    private List<ModelReportId> applicableReports;
     private List<String> applicableProjections;
 
-    public ProjectionDataSet(String dataSetName, String dataSetShortName, String inputType, String applicableProjection, List<ReportDataItem> data, List<String> applicableProjections) {
+    public ProjectionDataSet(String dataSetName, String dataSetShortName, String inputType, List<String> applicableProjections, List<ModelReportId> applicableReports, List<ReportDataItem> data) {
        super(dataSetName, dataSetShortName, inputType, data);
-        this.applicableProjection = applicableProjection;
-        this.applicableProjections = null==applicableProjections? Collections.emptyList():applicableProjections;
+        this.applicableReports = applicableReports == null ? Collections.emptyList() : applicableReports;
+        this.applicableProjections = applicableProjections == null ? Collections.emptyList() : applicableProjections;
     }
 
     public ProjectionDataSet() {
     }
 
-    public String getApplicableProjection() {
-        return applicableProjection;
+    public List<ModelReportId> getApplicableReports() {
+        return applicableReports;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ProjectionDataSet)) return false;
-        if (!super.equals(o)) return false;
-        ProjectionDataSet that = (ProjectionDataSet) o;
-        return Objects.equals(getApplicableProjection(), that.getApplicableProjection()) && Objects.equals(getApplicableProjections(), that.getApplicableProjections());
+    public List<String> getApplicableProjections() {
+        return applicableProjections;
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getApplicableProjection());
-    }
-
     @Override
     public String toString() {
         return new StringJoiner(", ", ProjectionDataSet.class.getSimpleName() + "[", "]")
                 .add("dataSetName='" + getDataSetName() + "'")
                 .add("inputType='" + getInputType() + "'")
                 .add("expectedType='" + getExpectedType() + "'")
-                .add("applicableProjection=" + applicableProjection)
+                .add("applicableReports=" + applicableReports)
                 .add("applicableProjections=" + applicableProjections)
                 .add("data=" + getData())
                 .toString();
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ProjectionDataSet that = (ProjectionDataSet) o;
+        return Objects.equals(applicableProjections, that.applicableProjections) && Objects.equals(applicableReports, that.applicableReports);
+    }
 
-    public List<String> getApplicableProjections() {
-
-        List<String> applicableProjectionsAsList = new ArrayList<>();
-        if(null!=this.applicableProjections) {
-            applicableProjectionsAsList.addAll(this.applicableProjections);
-        }
-        if(null != getApplicableProjection() && !applicableProjectionsAsList.contains(getApplicableProjection())){
-            applicableProjectionsAsList.add(getApplicableProjection());
-        }
-        return applicableProjectionsAsList;
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), applicableProjections, applicableReports);
     }
 }

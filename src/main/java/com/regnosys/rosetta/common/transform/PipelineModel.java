f6e19e741f74afe347f8468d11379f1ec635d9b1
PayalKhanna <payal.khanna@regnosys.com>
2024-04-15 14:26:56 +0100

    task-4336: move PipelineModel.java and TestPackModel.java classes to common

--- src/main/java/com/regnosys/rosetta/common/transform/PipelineModel.java
+++ src/main/java/com/regnosys/rosetta/common/transform/PipelineModel.java
@@ -1,2 +1,13 @@
 package com.regnosys.rosetta.common.transform;
 
+public record PipelineModel(String id, String name, Transform transform, String upstreamPipelineId, Serialisation outputSerialisation) {
+    public record Transform(TransformType type, String function, String inputType, String outputType) {}
+
+    public record Serialisation(Format format, String configPath) {
+        public enum Format {
+            JSON,
+            XML
+        }
+    }
+}
+
--- src/main/java/com/regnosys/rosetta/common/transform/TestPackModel.java
+++ src/main/java/com/regnosys/rosetta/common/transform/TestPackModel.java
@@ -1,2 +1,9 @@
 package com.regnosys.rosetta.common.transform;
 
+import java.util.List;
+
+public record TestPackModel(String id, String pipelineId, String name, List<SampleModel> samples) {
+    public record SampleModel(String id, String name, String inputPath, String outputPath, String outputTabulatedPath, Assertions assertions) {
+        public record Assertions(Integer modelValidationFailures, Boolean schemaValidationFailure, Boolean runtimeError) {}
+    }
+}

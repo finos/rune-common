b907f31953eb0f881374b3870b55ef048db69880
PayalKhanna <payal.khanna@regnosys.com>
2024-04-19 11:47:50 +0100

    task-4336: testpackutils

---
+++ src/main/java/com/regnosys/rosetta/common/transform/TestPackUtils.java
@@ -0,0 +1,24 @@
+package com.regnosys.rosetta.common.transform;
+
+import java.util.List;
+
+public class TestPackUtils {
+    public static TestPackModel createTestPack(String dataSetName, TransformType transformType, String formattedFunctionName, List<TestPackModel.SampleModel> sampleModels) {
+        return new TestPackModel(
+                createTestPackId(transformType, formattedFunctionName, dataSetName),
+                createPipelineId(transformType, formattedFunctionName),
+                dataSetName,
+                sampleModels
+        );
+    }
+
+    private static String createTestPackId(TransformType transformType, String formattedFunctionName, String dataSetName) {
+        return String.format("test-pack-%s-%s-%s", transformType.name().toLowerCase(), formattedFunctionName, dataSetName.toLowerCase());
+    }
+
+
+    private static String createPipelineId(TransformType transformType, String formattedFunctionName) {
+        return String.format("pipeline-%s-%s", transformType.name().toLowerCase(), formattedFunctionName);
+    }
+
+}

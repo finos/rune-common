0b4a049d432b85115bffcd4caa400609b7a3e7d3
PayalKhanna <payal.khanna@regnosys.com>
2024-05-15 16:08:40 +0100

    task-4317: removed deprecated and outputTabulatedPath

--- src/main/java/com/regnosys/rosetta/common/reports/ReportField.java
+++ src/main/java/com/regnosys/rosetta/common/reports/ReportField.java
@@ -9,57 +9,58 @@ import java.util.StringJoiner;
 @Deprecated
 public class ReportField implements Comparable<ReportField> {
 
-	private final String name;
-	private final String rule;
-	private final Integer repeatableIndex;
-	private final String value;
-	private final String issue;
+    private final String name;
+    private final String rule;
+    private final Integer repeatableIndex;
+    private final String value;
+    private final String issue;
 
-	@JsonCreator
-	public ReportField(@JsonProperty("name") String name,
-					   @JsonProperty("rule") String rule,
-					   @JsonProperty("repeatableIndex") Integer repeatableIndex,
-					   @JsonProperty("value") String value,
-					   @JsonProperty("issue") String issue) {
-		this.name = name;
-		this.rule = rule;
-		this.repeatableIndex = repeatableIndex;
-		this.value = value;
-		this.issue = issue;
-	}
+    @JsonCreator
+    public ReportField(@JsonProperty("name") String name,
+                       @JsonProperty("rule") String rule,
+                       @JsonProperty("repeatableIndex") Integer repeatableIndex,
+                       @JsonProperty("value") String value,
+                       @JsonProperty("issue") String issue) {
+        this.name = name;
+        this.rule = rule;
+        this.repeatableIndex = repeatableIndex;
+        this.value = value;
+        this.issue = issue;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getRule() {
-		return rule;
-	}
+    public String getRule() {
+        return rule;
+    }
 
-	public Integer getRepeatableIndex() {
-		return repeatableIndex;
-	}
+    public Integer getRepeatableIndex() {
+        return repeatableIndex;
+    }
 
-	public String getValue() {
-		return value;
-	}
+    public String getValue() {
+        return value;
+    }
 
-	public String getIssue() {
-		return issue;
-	}
+    public String getIssue() {
+        return issue;
+    }
 
-	@Override public String toString() {
-		return new StringJoiner(", ", ReportField.class.getSimpleName() + "[", "]")
-				.add("name='" + name + "'")
-				.add("rule='" + rule + "'")
-				.add("repeatableIndex=" + repeatableIndex)
-				.add("value='" + value + "'")
-				.add("issue='" + issue + "'")
-				.toString();
-	}
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ReportField.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("rule='" + rule + "'")
+                .add("repeatableIndex=" + repeatableIndex)
+                .add("value='" + value + "'")
+                .add("issue='" + issue + "'")
+                .toString();
+    }
 
-	@Override
-	public int compareTo(ReportField o) {
-		return Comparator.comparing(ReportField::getName).compare(this, o);
-	}
+    @Override
+    public int compareTo(ReportField o) {
+        return Comparator.comparing(ReportField::getName).compare(this, o);
+    }
 }

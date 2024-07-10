80bcb345e608172ee50514acea17b11bf4710383
David Al-Kanani <david.al-kanani@regnosys.com>
2024-07-10 17:29:34 +0100

    deprecate TransformType.getTabulatorName method (#401)
    * deprecate TransformType.getTabulatorName method
    
    * add deprecation javadoc

--- src/main/java/com/regnosys/rosetta/common/transform/TransformType.java
+++ src/main/java/com/regnosys/rosetta/common/transform/TransformType.java
@@ -23,7 +23,7 @@ package com.regnosys.rosetta.common.transform;
 import java.util.function.Function;
 
 public enum TransformType {
-    TRANSLATE("translate", "TBD"),
+    TRANSLATE("translate", null),
     PROJECTION("projection", "projections.%sProjectionTabulator"),
     REPORT("regulatory-reporting", "reports.%sReportTabulator", stripReportFunctionName()),
     ENRICH("enrich", null);
@@ -53,7 +53,14 @@ public enum TransformType {
         return resourcePath;
     }
 
+    /**
+     * @deprecated custom tabulator names have been deprecated in favour of having a common naming convention for tabulators
+     */
+    @Deprecated
     public String getTabulatorName(String functionName) {
+        if (tabulatorName == null) {
+            throw new UnsupportedOperationException(String.format("Cannot get tabulator name from TransformType.%s", name()));
+        }
         return String.format(tabulatorName, transformFunctionName.apply(functionName));
     }
 

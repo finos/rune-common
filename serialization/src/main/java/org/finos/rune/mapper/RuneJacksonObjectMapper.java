50c0b00bbb4dd14a96cfab2b23ed832e2f48baa3
David Al-Kanani <david.al-kanani@regnosys.com>
2024-12-23 12:13:33 +0000

    add object mapper initial

---
+++ serialization/src/main/java/org/finos/rune/mapper/RuneJacksonObjectMapper.java
@@ -0,0 +1,44 @@
+package org.finos.rune.mapper;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.rosetta.model.lib.RosettaModelObject;
+
+public class RuneJacksonObjectMapper extends ObjectMapper {
+    public RuneJacksonObjectMapper() {
+        super(create());
+    }
+
+    private static ObjectMapper create() {
+        return new ObjectMapper()
+                .registerModule(new GuavaModule())
+                .registerModule(new JodaModule())
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule())
+                .registerModule(new RosettaDateModule())
+                .registerModule(new MyRosettaJSONModule(true))
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+                .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
+                .configure(SerializationFeature.WRITE_DATES_WITH_CONTEXT_TIME_ZONE, false)
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
+                .setFilterProvider(new SimpleFilterProvider().addFilter("SubTypeFilter", new SubTypeFilter()))
+                .addMixIn(RosettaModelObject.class, RossetaModelObjectMixin.class)
+                .enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
+                .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.PUBLIC_ONLY);
+    }
+}
---
+++ serialization/src/main/java/org/finos/rune/mapper/mixins/RosettaModelObjectMixin.java
@@ -0,0 +1,12 @@
+package org.finos.rune.mapper.mixins;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.model.lib.annotations.RuneAttribute;
+
+@JsonFilter("SubTypeFilter")
+public @interface RosettaModelObjectMixin {
+
+    @RuneAttribute("@type")
+    Class<? extends RosettaModelObject> getType();
+}

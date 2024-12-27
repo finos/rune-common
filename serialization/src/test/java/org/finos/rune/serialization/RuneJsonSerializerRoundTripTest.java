e25d9c3a42d273af72243f55fc68e6917e8924e2
David Al-Kanani <david.al-kanani@regnosys.com>
2024-12-27 13:07:16 +0000

    refactor injector

--- serialization/src/test/java/org/finos/rune/serialization/RuneJsonSerializerRoundTripTest.java
+++ serialization/src/test/java/org/finos/rune/serialization/RuneJsonSerializerRoundTripTest.java
@@ -21,7 +21,10 @@ package org.finos.rune.serialization;
  */
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.*;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 import com.google.inject.util.Modules;
 import com.regnosys.rosetta.RosettaRuntimeModule;
 import com.regnosys.rosetta.RosettaStandaloneSetup;
@@ -30,7 +33,6 @@ import com.regnosys.rosetta.config.RosettaGeneratorsConfiguration;
 import com.regnosys.rosetta.config.RosettaModelConfiguration;
 import com.regnosys.rosetta.tests.util.CodeGeneratorTestHelper;
 import com.rosetta.model.lib.RosettaModelObject;
-import com.rosetta.model.lib.process.PostProcessor;
 import org.eclipse.xtext.common.TerminalsStandaloneSetup;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -55,21 +57,7 @@ public class RuneJsonSerializerRoundTripTest {
 
     @BeforeAll
     static void beforeAll() {
-        RosettaStandaloneSetup rosettaStandaloneSetup = new RosettaStandaloneSetup();
-        TerminalsStandaloneSetup.doSetup();
-
-        Module module = Modules.override(new RosettaRuntimeModule()).with(new AbstractModule() {
-            @Override
-            protected void configure() {
-                bind(RosettaConfiguration.class).toInstance(new RosettaConfiguration(
-                        new RosettaModelConfiguration("test"),
-                        new ArrayList<>(),
-                        new RosettaGeneratorsConfiguration()
-                ));
-            }
-        });
-        Injector injector = Guice.createInjector(module);
-        rosettaStandaloneSetup.register(injector);
+        Injector injector = setupInjector();
         helper = injector.getInstance(CodeGeneratorTestHelper.class);
         dynamicCompiledClassLoader = new DynamicCompiledClassLoader();
     }
@@ -105,6 +93,25 @@ public class RuneJsonSerializerRoundTripTest {
                                     ));
                         }
                 );
+    }
+
+    private static Injector setupInjector() {
+        RosettaStandaloneSetup rosettaStandaloneSetup = new RosettaStandaloneSetup();
+        TerminalsStandaloneSetup.doSetup();
+
+        Module module = Modules.override(new RosettaRuntimeModule()).with(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(RosettaConfiguration.class).toInstance(new RosettaConfiguration(
+                        new RosettaModelConfiguration("test"),
+                        new ArrayList<>(),
+                        new RosettaGeneratorsConfiguration()
+                ));
+            }
+        });
+        Injector injector = Guice.createInjector(module);
+        rosettaStandaloneSetup.register(injector);
+        return injector;
     }
 
 }

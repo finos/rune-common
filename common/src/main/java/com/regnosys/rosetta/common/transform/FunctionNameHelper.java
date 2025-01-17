822d9d49dcb165ccc6d061cf01359032845d668b
Chuks-Ajeh <chuks.ajeh@regnosys.com>
2025-01-17 14:34:44 +0000

    task-5580: updated licensing

--- common/src/main/java/com/regnosys/rosetta/common/transform/FunctionNameHelper.java
+++ common/src/main/java/com/regnosys/rosetta/common/transform/FunctionNameHelper.java
@@ -1,11 +1,11 @@
 package com.regnosys.rosetta.common.transform;
 
 /*-
- * ===============
- * Rune Testing
- * ===============
- * Copyright (C) 2022 - 2024 REGnosys
- * ===============
+ * ==============
+ * Rune Common
+ * ==============
+ * Copyright (C) 2018 - 2024 REGnosys
+ * ==============
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,7 @@ package com.regnosys.rosetta.common.transform;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ===============
+ * ==============
  */
 
 import com.google.common.base.CaseFormat;

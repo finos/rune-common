16f2ecd3b45977bced5155c425559068c112b3b3
David Al-Kanani <david.al-kanani@regnosys.com>
2025-02-10 10:14:25 +0000

    build reference collector

--- serialization/src/main/java/org/finos/rune/mapper/processor/GlobalReferenceCollector.java
+++ serialization/src/main/java/org/finos/rune/mapper/processor/GlobalReferenceCollector.java
@@ -1,34 +1,44 @@
 package org.finos.rune.mapper.processor;
 
 import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.model.lib.meta.ReferenceWithMeta;
 import com.rosetta.model.lib.path.RosettaPath;
 import com.rosetta.model.lib.process.AttributeMeta;
 import com.rosetta.model.lib.process.Processor;
 
-import java.util.*;
-
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GlobalReferenceCollector implements Processor {
-    //NOTE WE CAN'T KEY JUST WITH REFERENCE VALUES: a global key is a hash on only the value so to identify it you need it's type and key value
     private final Set<GlobalReferenceRecord> globalReferences = new HashSet<>();
+
     @Override
     public <R extends RosettaModelObject> boolean processRosetta(RosettaPath path, Class<? extends R> rosettaType, R instance, RosettaModelObject parent, AttributeMeta... metas) {
-        return false;
+        if (instance instanceof ReferenceWithMeta) {
+            @SuppressWarnings("unchecked")
+            ReferenceWithMeta<R> reference = (ReferenceWithMeta<R>) instance;
+            Class<?> referenceValueType = getReferenceValueType(instance);
+            String referenceKeyValue = reference.getGlobalReference();
+            globalReferences.add(new GlobalReferenceRecord(referenceValueType, referenceKeyValue));
+        }
+        return true;
     }
 
     @Override
     public <R extends RosettaModelObject> boolean processRosetta(RosettaPath path, Class<? extends R> rosettaType, List<? extends R> instance, RosettaModelObject parent, AttributeMeta... metas) {
-        return false;
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public <T> void processBasic(RosettaPath path, Class<? extends T> rosettaType, T instance, RosettaModelObject parent, AttributeMeta... metas) {
-
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
     public <T> void processBasic(RosettaPath path, Class<? extends T> rosettaType, Collection<? extends T> instance, RosettaModelObject parent, AttributeMeta... metas) {
-
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public Set<GlobalReferenceRecord> getGlobalReferences() {
@@ -40,4 +50,10 @@ public class GlobalReferenceCollector implements Processor {
         throw new UnsupportedOperationException("Report not supported for UnreferencedKeyCollector");
     }
 
+    private Class<?> getReferenceValueType(RosettaModelObject rosettaModelObject) {
+        if (rosettaModelObject instanceof ReferenceWithMeta) {
+            return ((ReferenceWithMeta<?>) rosettaModelObject).getValueType();
+        }
+        return rosettaModelObject.getType();
+    }
 }
--- serialization/src/main/java/org/finos/rune/mapper/processor/GlobalReferenceRecord.java
+++ serialization/src/main/java/org/finos/rune/mapper/processor/GlobalReferenceRecord.java
@@ -1,11 +1,25 @@
 package org.finos.rune.mapper.processor;
 
+import java.util.Objects;
+
 public class GlobalReferenceRecord {
     public final Class<?> referenceOnType;
-    public final String referenceValue;
+    public final String referenceKeyValue;
 
-    public GlobalReferenceRecord(Class<?> referenceOnType, String referenceValue) {
+    public GlobalReferenceRecord(Class<?> referenceOnType, String referenceKeyValue) {
         this.referenceOnType = referenceOnType;
-        this.referenceValue = referenceValue;
+        this.referenceKeyValue = referenceKeyValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GlobalReferenceRecord that = (GlobalReferenceRecord) o;
+        return Objects.equals(referenceOnType, that.referenceOnType) && Objects.equals(referenceKeyValue, that.referenceKeyValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(referenceOnType, referenceKeyValue);
     }
 }

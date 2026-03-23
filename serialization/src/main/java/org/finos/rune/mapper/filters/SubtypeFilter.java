d5f01effe5a7ece32471735872634aad4847d074
David Al-Kanani <david.al-kanani@regnosys.com>
2026-03-23 10:41:38 +0000

    Fix polymorphic serialisation on the Rune serialiser (#584)
    * Include @type conditionally for polymorphic deserialization and enable related test.
    
    * Refactor `SubtypeFilter` to improve polymorphic serialization logic and simplify method resolution.
    
    * Rename `fieldName` to `propertyName` in `SubtypeFilter` for clarity.
    
    * Add logging to `SubtypeFilter` and simplify polymorphic serialization logic.
    
    * Remove unused logger and simplify `SubtypeFilter` serialization logic.

--- serialization/src/main/java/org/finos/rune/mapper/filters/SubtypeFilter.java
+++ serialization/src/main/java/org/finos/rune/mapper/filters/SubtypeFilter.java
@@ -9,9 +9,9 @@ package org.finos.rune.mapper.filters;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,29 +22,72 @@ package org.finos.rune.mapper.filters;
 
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonStreamContext;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.PropertyWriter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.rosetta.model.lib.RosettaModelObject;
+import com.rosetta.model.lib.annotations.RuneAttribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class SubtypeFilter extends SimpleBeanPropertyFilter {
 
     @Override
     public void serializeAsField(Object pojo, JsonGenerator jgen, SerializerProvider provider, PropertyWriter writer) throws Exception {
+        if (!(pojo instanceof RosettaModelObject)) {
+            super.serializeAsField(pojo, jgen, provider, writer);
+            return;
+        }
+
         String name = writer.getName();
         if (name.equals("@type")) {
             /*
-                TODO: find a better way to add json type fields.
-                The below approach doesn't work and adds @type in too many places. An alternative approach
-                may be to stop generating @type in the POJOs and only add them where needed in
-                the RuneJsonAnnotationIntrospector by overriding findAndAddVirtualProperties
+                Include @type only when needed for polymorphic deserialization:
+                - When the object's runtime class is a subtype AND
+                - The object is being serialized into a property expecting a base type
+                This allows proper deserialization of subtypes when assigned to base type fields
              */
-//            if (!pojo.getClass().getSuperclass().getCanonicalName().equals(Object.class.getCanonicalName())) {
-//                writer.serializeAsField(pojo, jgen, provider);
-//            }
+            Class<?> runtimeClass = pojo.getClass();
+            Class<?> superclass = runtimeClass.getSuperclass();
+
+            if (superclass != null && !superclass.equals(Object.class)) {
+                JsonStreamContext parentContext = jgen.getOutputContext().getParent();
+                if (parentContext != null) {
+                    String propertyName = parentContext.getCurrentName();
+                    Object parentObject = parentContext.getCurrentValue();
+
+                    if (propertyName != null && parentObject != null) {
+                        Class<?> parentClass = parentObject.getClass();
+                        Method getter = findMethod(parentClass, propertyName);
+
+                        if (getter != null) {
+                            Class<?> declaredType = getter.getReturnType();
+                            if (declaredType.isAssignableFrom(runtimeClass)) {
+                                Class<? extends RosettaModelObject> type = ((RosettaModelObject) pojo).getType();
+                                if (declaredType.equals(type)) {
+                                    return;
+                                }
+                                writer.serializeAsField(pojo, jgen, provider);
+                            }
+                        }
+                    }
+                }
+            }
             return;
         }
         super.serializeAsField(pojo, jgen, provider, writer);
     }
 
-
+    private Method findMethod(Class<?> clazz, String propertyName) {
+        for (Method method : clazz.getMethods()) {
+            RuneAttribute annotation = method.getAnnotation(RuneAttribute.class);
+            if (annotation != null && annotation.value().equals(propertyName)) {
+                return method;
+            }
+        }
+        return null;
+    }
 }
